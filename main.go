d8dfc6a198e2dbb9246124a1f8e75a21f9d27818
Jackneill <general.jackneill@gmail.com>
2013-07-01 13:32:49 +0200

    menu changes

--- main.go
+++ main.go
@@ -18,6 +18,7 @@ var config map[string]interface{}
 
 type Page struct {
 	Title string
+	Name  string
 	Body  string
 }
 
@@ -62,6 +63,9 @@ func loadConfig() {
 
 func loadServer(port string) {
 	fmt.Print("Starting web server on localhost", port, "...\n")
+	funcMap := template.FuncMap{
+		"equal": equal,
+	}
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		/*var err error
 		_, err = db.Exec("insert into admins(ServerName, Name, Password, Vhost) values('tesztszerver', 'tesztName', 'tesztpassword', 'tesztVhost')")
@@ -90,26 +94,58 @@ func loadServer(port string) {
 			names = append(names, e.Value.(string))
 		}*/
 
-		t, _ := template.New("index.tpl").ParseFiles("www/template/menu.tpl", "www/template/header.tpl", "www/template/footer.tpl", "www/index.tpl")
-		p := Page{Title: "Shumix Webadmin", Body: "works"}
-		t.Execute(w, p)
+		t, err := template.New("index.tpl").Funcs(funcMap).ParseFiles("www/template/menu.tpl", "www/template/header.tpl", "www/template/footer.tpl", "www/index.tpl")
+		if err != nil {
+			fmt.Println(err)
+		}
+		p := Page{Title: config["webtitle"].(string), Name: "index", Body: "works"}
+		err = t.Execute(w, p)
+		if err != nil {
+			fmt.Println(err)
+		}
 	})
 	http.HandleFunc("/login", func(w http.ResponseWriter, r *http.Request) {
-		t, _ := template.New("signin.tpl").ParseFiles("www/template/menu.tpl", "www/template/header.tpl", "www/template/footer.tpl", "www/signin.tpl")
-		p := Page{Title: "Login - Schumix Webserver", Body: "works"}
-		t.Execute(w, p)
+		t, err := template.New("signin.tpl").Funcs(funcMap).ParseFiles("www/template/menu.tpl", "www/template/header.tpl", "www/template/footer.tpl", "www/signin.tpl")
+		if err != nil {
+			fmt.Println(err)
+		}
+		p := Page{Title: "Login - " + config["webtitle"].(string), Name: "login", Body: "works"}
+		err = t.Execute(w, p)
+		if err != nil {
+			fmt.Println(err)
+		}
 	})
 	http.HandleFunc("/about", func(w http.ResponseWriter, r *http.Request) {
-		t, _ := template.New("about.tpl").ParseFiles("www/template/menu.tpl", "www/template/header.tpl", "www/template/footer.tpl", "www/about.tpl")
-		p := Page{Title: "About - Schumix Webserver", Body: "works"}
-		t.Execute(w, p)
+		t, err := template.New("about.tpl").Funcs(funcMap).ParseFiles("www/template/menu.tpl", "www/template/header.tpl", "www/template/footer.tpl", "www/about.tpl")
+		if err != nil {
+			fmt.Println(err)
+		}
+		p := Page{Title: "About - " + config["webtitle"].(string), Name: "about", Body: "works"}
+		err = t.Execute(w, p)
+		if err != nil {
+			fmt.Println(err)
+		}
 	})
 	http.HandleFunc("/stats", func(w http.ResponseWriter, r *http.Request) {
-		t, _ := template.New("stats.tpl").ParseFiles("www/template/menu.tpl", "www/template/header.tpl", "www/template/footer.tpl", "www/stats.tpl")
-		p := Page{Title: "Public Stats - Schumix Webserver", Body: "works"}
-		t.Execute(w, p)
+		t, err := template.New("stats.tpl").Funcs(funcMap).ParseFiles("www/template/menu.tpl", "www/template/header.tpl", "www/template/footer.tpl", "www/stats.tpl")
+		if err != nil {
+			fmt.Println(err)
+		}
+		p := Page{Title: "Public Stats - " + config["webtitle"].(string), Name: "stats", Body: "works"}
+		err = t.Execute(w, p)
+		if err != nil {
+			fmt.Println(err)
+		}
 	})
 	fmt.Print("Done. Serving...\n")
 	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("www/static/"))))
 	http.ListenAndServe(port, nil)
+}
+
+// In .tpl files equality check
+func equal(a, b interface{}) bool {
+	if a == b {
+		return true
+	}
+	return false
 }
--- www/template/menu.tpl
+++ www/template/menu.tpl
@@ -1,14 +1,14 @@
 {{define "menu"}}
       <div class="masthead">
-        <h3 class="muted">Schumix Webadmin</h3>
+        <h3 class="muted">{{.Title}}</h3>
         <div class="navbar">
           <div class="navbar-inner">
             <div class="container">
               <ul class="nav">
-                <li class="active"><a href="/">Home</a></li>
-                <li><a href="/stats">Statistics</a></li>
-                <li><a href="/about">About</a></li>
-                <li><a href="/login">Login</a></li>
+                <li {{if equal .Name "index"}}class="active"{{end}}><a href="/">Home</a></li>
+                <li {{if equal .Name "stats"}}{{else}}class="active"{{end}}><a href="/stats">Statistics</a></li>
+                <li {{if equal .Name "about"}}class="active"{{end}}><a href="/about">About</a></li>
+                <li {{if equal .Name "login"}}class="active"{{end}}><a href="/login">Login</a></li>
               </ul>
             </div>
           </div>
