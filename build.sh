6802403641f484b59fede18274938350664af7a9
Megax <megax@yeahunter.hu>
2013-07-21 10:45:46 +0200

    * build.sh hozzáadva a forráshoz. Így egyszerűen és gyorsan le leeht fordítani a programot. A kész program a bin mappába kerül.

---
+++ build.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+sourcedir=`ls -d $PWD`
+gopath="$sourcedir/.gopath"
+bin="$sourcedir/bin"
+
+if [ ! -e $gopath ]; then
+	mkdir $gopath
+fi
+
+export GOPATH=$gopath
+
+go get github.com/mattn/go-sqlite3
+go get github.com/hoisie/web
+go get github.com/mattn/go-session-manager
+go get github.com/Jackneill/gosemver
+
+go build
+
+if [ ! -e $bin ]; then
+	mkdir $bin
+fi
+
+mv Webadmin $bin/Webadmin
